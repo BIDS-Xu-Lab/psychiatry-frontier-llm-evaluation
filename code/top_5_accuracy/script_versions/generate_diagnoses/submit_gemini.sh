f69b6b6706bb6f7c3557a43e47ccd5482e8588a6
kevinwjin <kevin.defan@gmail.com>
2026-01-06 00:58:39 -0500

    Fix Slurm scripts

--- code/top_5_accuracy/script_versions/generate_diagnoses/submit_gemini.sh
+++ code/top_5_accuracy/script_versions/generate_diagnoses/submit_gemini.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mem=32G
 #SBATCH --partition=day
-#SBATCH --output=/gpfs/radev/project/xu_hua/kwj9/psychiatry_llm/evaluation_of_sota/jama_submission_12152025/psychiatry-frontier-llm-evaluation/code/top_5_accuracy/script_versions/slurm_logs/%x-%j.out
+#SBATCH --output=/gpfs/radev/project/xu_hua/kwj9/psychiatry_llm/evaluation_of_sota/jama_submission_12152025/psychiatry-frontier-llm-evaluation/code/top_5_accuracy/script_versions/generate_diagnoses/slurm_logs/%x-%j.out
 
 module reset
 module load miniconda
