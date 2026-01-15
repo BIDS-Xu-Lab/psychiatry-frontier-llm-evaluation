561290bb9f6fceaa3ea157590d5ce0a2e32addd8
kevinwjin <kevin.defan@gmail.com>
2026-01-14 21:11:15 -0500

    Update plots

--- analysis/table_1_diagnostic_accuracy.R
+++ analysis/table_1_diagnostic_accuracy.R
@@ -18,6 +18,8 @@ combined_results <- rbind(
   data.frame(Model = "Anthropic Claude Opus 4.5", claude_opus_4.5)
 ) |> as_tibble()
 
+
+#### Long format table ####
 # Sort data by score
 combined_results <-
   combined_results |>
@@ -32,7 +34,7 @@ gt_results <- gt(combined_results,
 gt_results <-
   gt_results |>
   tab_header(
-    title = "Psychiatric Diagnostic Performance of Frontier Large Language Models",
+    title = "Psychiatric Diagnostic Accuracy of Large Language Models",
     subtitle = md("Based on ability to diagnose psychiatric case vignettes (*n* = 196)")
   )
 
@@ -70,7 +72,7 @@ gt_results <-
   ) |>
   tab_footnote(
     footnote = "Did the model identify at least one correct diagnosis among its 5 predictions?",
-    locations = cells_stub(rows = c("Top-5 Hit Rate"))
+    locations = cells_stub(rows = c("Top-5 Accuracy"))
   ) |>
   tab_footnote(
     footnote = "Percentage of patient's total diagnoses identified by the model.",
@@ -106,4 +108,112 @@ gt_results
 
 # Export as PNG
 setwd("~/Documents/School/PhD (Yale)/PhD/xu_lab/Repositories/psychiatry-frontier-llm-evaluation/analysis")
-gtsave(gt_results, filename = "table_1.png")
+gtsave(gt_results, filename = "table_1_long.png")
+
+
+
+#### Wide format table ####
+# Pivot models values into their own columns
+combined_results_wide <-
+  combined_results |>
+  pivot_wider(names_from = Model, values_from = Score) |>
+  arrange(Metric)
+
+gt_results_wide <- gt(combined_results_wide)
+
+# Add header
+gt_results_wide <-
+  gt_results_wide |>
+  tab_header(
+    title = "Psychiatric Diagnostic Accuracy of Large Language Models",
+    subtitle = md("Based on ability to diagnose psychiatric case vignettes (*n* = 196)")
+  )
+
+# Round scores to 3 decimal places
+gt_results_wide <- 
+  gt_results_wide |>
+  fmt_number(columns = everything(),
+             decimals = 3)
+
+# Order columns by score
+gt_results_wide <-
+  gt_results_wide |>
+  cols_move(
+    columns = "Google Gemini 3 Pro",
+    after = "OpenAI GPT-5.2"
+  )
+
+# Relabel columns
+gt_results_wide <-
+  gt_results_wide |>
+  cols_label(
+    c("Anthropic Claude Opus 4.5") ~ md("Claude Opus 4.5"),
+    c("Google Gemini 3 Pro") ~ md("Gemini 3 Pro"),
+    c("OpenAI GPT-5.2") ~ md("GPT-5.2"),
+    c("Metric") ~ ""  # Remove "Metric" label
+  )
+
+# Add footnotes
+gt_results_wide <-
+  gt_results_wide |>
+  tab_footnote(
+    footnote = md("Models accessed via respective provider APIs in December 2025."),
+    locations = cells_title(groups = "title")
+  ) |>
+  tab_footnote(
+    footnote = md("Rounded to three decimal places."),
+    locations = cells_title(groups = "subtitle")
+  ) |>
+  tab_footnote(
+    footnote = "Did the model identify the correct primary diagnosis?",
+    locations = cells_body(
+      columns = "Metric",
+      rows = 3
+      )
+  ) |>
+  tab_footnote(
+    footnote = "Did the model identify at least one correct diagnosis among its 5 predictions?",
+    locations = cells_body(
+      columns = "Metric",
+      rows = 4
+    )
+  ) |>
+  tab_footnote(
+    footnote = "Percentage of patient's total diagnoses identified by the model.",
+    locations = cells_body(
+      columns = "Metric",
+      rows = 2
+    )
+  ) |>
+  tab_footnote(
+    footnote = "How high up did the model's first correct diagnosis appear?",
+    locations = cells_body(
+      columns = "Metric",
+      rows = 1
+    )
+  ) |>
+  opt_footnote_marks(marks = "standard") |>
+  tab_options(
+    footnotes.font.size = "8pt" # Or use specific points like "8pt"
+  )
+
+# Style cells based on value
+gt_results_wide <-
+  gt_results_wide |>
+  # Highlight top-scoring model
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_body(
+      columns = "Anthropic Claude Opus 4.5"
+      )
+    ) |>
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels(
+      columns = "Anthropic Claude Opus 4.5"
+    ) 
+  ) 
+
+gt_results_wide
+setwd("~/Documents/School/PhD (Yale)/PhD/xu_lab/Repositories/psychiatry-frontier-llm-evaluation/analysis")
+gtsave(gt_results_wide, filename = "table_1_wide.png")
